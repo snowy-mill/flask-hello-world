aedb5ab90cc5b0db2fc1e3fd28c02b0429356883
nasser <nasser.alkhteeb12@gmail.com>
2022-12-30 07:51:41 +0800

    made profile avatar

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 from app import login
-
+from hashlib import md5
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,6 +20,10 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return "<user {}>".format(self.username)
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('UTF-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 
 class Post(db.Model):
