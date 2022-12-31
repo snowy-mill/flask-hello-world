ae8e8a559e68a4303ddc7c1ab725a70c5b952108
Nil <nasser.alkhteeb12@gmail.com>
2022-12-31 08:41:02 +0800

    email server, log file
    wrote code that makes an email server to email me errors once they occur and a log file. fixed the multiple usernames issue; however, the code is unintelligible to me

--- app/routes.py
+++ app/routes.py
@@ -7,11 +7,14 @@ from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from werkzeug.urls import url_parse
 from datetime import datetime
 
+
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+
+
 @app.route('/')
 @app.route('/index')
 @login_required
@@ -50,10 +53,14 @@ def login():
             next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title="login form", form=form)
+
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
@@ -67,16 +74,20 @@ def register():
         flash("congradulations, you are now a registered user!")
         return redirect(url_for('login'))
     return render_template('register.html', title="Registration", form=form)
+
+
 @app.route('/user/<username>')
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = [{'author': user, 'body': 'test post #1'}, {'author': user, 'body': 'test post #2'}]
     return render_template('user.html', user=user, posts=posts)
+
+
 @app.route('/edit_profile', methods=['POST', 'GET'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
